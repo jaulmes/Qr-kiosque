f56db30b16de36545fc7d6dc123279623f9599d6
jaulmes <anoudemj@gmail.com>
2025-10-24 08:58:08 +0100

    ajout de la commande pour excecuter les taches cron sur hostinger

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+
+// Votre commande pour exécuter les jobs se place ici :
+Schedule::command('queue:work --stop-when-empty')
+         ->everyMinute()
+         ->withoutOverlapping();
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
